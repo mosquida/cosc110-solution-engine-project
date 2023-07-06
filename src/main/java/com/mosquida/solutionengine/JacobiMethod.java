20735c2f5db9117046f068ce744c09b41341b02a
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2023-07-06 14:35:16 +0800

    Added stopping point at jacobi - stop when row same

--- src/main/java/com/mosquida/solutionengine/JacobiMethod.java
+++ src/main/java/com/mosquida/solutionengine/JacobiMethod.java
@@ -77,6 +77,9 @@ public class JacobiMethod implements Initializable {
     @FXML
     private TableColumn<JacobiModel, String> jacobi_z;
 
+    @FXML
+    private TableView<JacobiModel> table;
+
     ObservableList<JacobiModel> jacobiList = FXCollections.observableArrayList();
 
     @Override
@@ -90,13 +93,7 @@ public class JacobiMethod implements Initializable {
 
         table.setItems(jacobiList);
     }
-    
-    @FXML
-    private TableView<JacobiModel> table;
-    
-    
-    
-    
+
     @FXML
     void onSolveClick() {
         // Reset Table
@@ -110,30 +107,28 @@ public class JacobiMethod implements Initializable {
         String c1 = reverseSign(new BigDecimal(c1_input.getText()));
         String d1 = d1_input.getText(); ///
 
-        System.out.println(c1_input.getText());
-        System.out.println(reverseSign(new BigDecimal(c1_input.getText())));
-        System.out.println(String.format("%s, %s, %s, %s",  a1, b1, c1, d1));
-
         String a2 = reverseSign(new BigDecimal(a2_input.getText()));
         String b2 = b2_input.getText(); ///
         String c2 = reverseSign(new BigDecimal(c2_input.getText()));
         String d2 = d2_input.getText(); ///
-        System.out.println(String.format("%s, %s, %s, %s",  a2, b2, c2, d2));
 
 
         String a3 = reverseSign(new BigDecimal(a3_input.getText()));
         String b3 = reverseSign(new BigDecimal(b3_input.getText()));
         String c3 = c3_input.getText(); ///
         String d3 = d3_input.getText(); ///
-        System.out.println(String.format("%s, %s, %s, %s",  a3, b3, c3, d3));
 
 
         BigDecimal x = new BigDecimal(0);
         BigDecimal y = new BigDecimal(0);
         BigDecimal z = new BigDecimal(0);
 
+        BigDecimal x_new = new BigDecimal(Double.MAX_VALUE);
+        BigDecimal y_new = new BigDecimal(Double.MAX_VALUE);
+        BigDecimal z_new = new BigDecimal(Double.MAX_VALUE);
+
         // TODO - add stopping point
-        while (i < 10) {
+        while (true) {
 
             String r1_formula = String.format("( (%s) +  (%s)y +  (%s)z)/ (%s)",d1, b1, c1, a1 );
             String r2_formula = String.format("( (%s) +  (%s)x +  (%s)z)/ (%s)",d2, a2, c2, b2 );
@@ -143,23 +138,29 @@ public class JacobiMethod implements Initializable {
             Argument r1b_arg = new Argument("y = " + y.toString());
             Argument r1c_arg = new Argument("z = " + z.toString());
             Expression expression1 = new Expression(r1_formula, r1b_arg, r1c_arg);
-            BigDecimal x_new  = BigDecimal.valueOf( expression1.calculate()).setScale(4, RoundingMode.HALF_UP);
+            x_new  = BigDecimal.valueOf( expression1.calculate()).setScale(4, RoundingMode.HALF_UP);
 
             Argument r2a_arg = new Argument("x = " + x.toString());
             Argument r2c_arg = new Argument("z = " + z.toString());
             Expression expression2 = new Expression(r2_formula, r2a_arg, r2c_arg);
-            BigDecimal y_new  = BigDecimal.valueOf( expression2.calculate()).setScale(4, RoundingMode.HALF_UP);
+            y_new  = BigDecimal.valueOf( expression2.calculate()).setScale(4, RoundingMode.HALF_UP);
 
             Argument r3a_arg = new Argument("x = " + x.toString());
             Argument r3b_arg = new Argument("y = " + y.toString());
             Expression expression3 = new Expression(r3_formula, r3a_arg, r3b_arg);
-            BigDecimal z_new  = BigDecimal.valueOf( expression3.calculate()).setScale(4, RoundingMode.HALF_UP);
+            z_new  = BigDecimal.valueOf( expression3.calculate()).setScale(4, RoundingMode.HALF_UP);
 
 //        mXparser.consolePrintln("Res 1: " + expression1.getExpressionString() + " = " + expression1.calculate());
 //        mXparser.consolePrintln("Res 2: " + expression2.getExpressionString() + " = " + expression2.calculate());
 //        mXparser.consolePrintln("Res 3: " + expression3.getExpressionString() + " = " + expression3.calculate());
-            System.out.println(String.format("%d, %s, %s, %s", i, x, y, z ));
+//        System.out.println(String.format("%d, %s, %s, %s", i, x, y, z ));
+
             jacobiList.add(new JacobiModel(i.toString(), x.toString(), y.toString(), z.toString()));
+
+            if (x.equals(x_new) && y.equals(y_new) &&z.equals(z_new)) {
+                break;
+            }
+
             x = x_new;
             y = y_new;
             z = z_new;
@@ -175,6 +176,5 @@ public class JacobiMethod implements Initializable {
         BigDecimal reversed = number.negate(); // Negate the number to reverse the sign
         String result = reversed.toString(); // Convert to string
         return result;
-
     }
 }
